e78a6c6ee2ee8de6ceba337c28c148b693e16669
eliezersamuel <eliezer_sss@hotmail.com>
2024-01-11 23:27:13 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './Pages/about/about.component';
+import { HomeComponent } from './Pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', title: 'Home page', component: HomeComponent},
+  {path: 'about', title: 'About page', component: AboutComponent}
+];
